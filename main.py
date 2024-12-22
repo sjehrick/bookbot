a30902300b4b2d482b716c4fe9970e774e255d43
sjehrick <samuel.ehrick@gmail.com>
2024-12-22 18:41:59 -0500

    Created main.py with word counting logic

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        print(len(words))
+        return len(words)
+
+
+main()
