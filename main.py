af0e64c9261d71beecd3d351e40d049753959734
sjehrick <samuel.ehrick@gmail.com>
2024-12-23 20:04:23 -0500

    Added split to lower string function

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    lower_string = make_string_lower(text)
     print(f"{num_words} words found in the document")
+    
 
 
 def get_num_words(text):
@@ -14,5 +16,10 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def make_string_lower(text):
+    split_string = text.split()
+    single_string = " ".join(split_string)
+    all_lower = single_string.lower()
+    return all_lower
 
 main()
