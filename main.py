ec8713ad7e8006af54eae4b1a688eb6e4027d98d
sjehrick <samuel.ehrick@gmail.com>
2024-12-23 20:53:53 -0500

    Created ugly letter counting function

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     lower_string = make_string_lower(text)
+    letter_dict = get_string_letters(lower_string)
     print(f"{num_words} words found in the document")
+    print(f"The total letters by the alphabet are: {letter_dict}")
     
-
 
 def get_num_words(text):
     words = text.split()
@@ -21,5 +22,39 @@ def make_string_lower(text):
     single_string = " ".join(split_string)
     all_lower = single_string.lower()
     return all_lower
+
+def get_string_letters(string):
+    alpha_dict = {
+        "a": 0,
+        "b": 0,
+        "c": 0,
+        "d": 0,
+        "e": 0,
+        "f": 0,
+        "g": 0,
+        "h": 0,
+        "i": 0,
+        "j": 0,
+        "j": 0,
+        "l": 0,
+        "m": 0,
+        "n": 0,
+        "o": 0,
+        "p": 0,
+        "q": 0,
+        "r": 0,
+        "s": 0,
+        "t": 0,
+        "u": 0,
+        "v": 0,
+        "w": 0,
+        "x": 0,
+        "y": 0,
+        "z": 0
+    }
+    for i in string:
+        if i in alpha_dict:
+            alpha_dict[i] += 1
+    return alpha_dict
 
 main()
