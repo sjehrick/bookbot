8335fe91c305ac099be0a13995075f502c07e282
sjehrick <samuel.ehrick@gmail.com>
2024-12-23 21:32:44 -0500

    Updated to the sample code. Was stumped :/

--- main.py
+++ main.py
@@ -2,59 +2,52 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    lower_string = make_string_lower(text)
-    letter_dict = get_string_letters(lower_string)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print(f"The total letters by the alphabet are: {letter_dict}")
-    
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
 
 def get_num_words(text):
     words = text.split()
     return len(words)
 
 
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def make_string_lower(text):
-    split_string = text.split()
-    single_string = " ".join(split_string)
-    all_lower = single_string.lower()
-    return all_lower
-
-def get_string_letters(string):
-    alpha_dict = {
-        "a": 0,
-        "b": 0,
-        "c": 0,
-        "d": 0,
-        "e": 0,
-        "f": 0,
-        "g": 0,
-        "h": 0,
-        "i": 0,
-        "j": 0,
-        "j": 0,
-        "l": 0,
-        "m": 0,
-        "n": 0,
-        "o": 0,
-        "p": 0,
-        "q": 0,
-        "r": 0,
-        "s": 0,
-        "t": 0,
-        "u": 0,
-        "v": 0,
-        "w": 0,
-        "x": 0,
-        "y": 0,
-        "z": 0
-    }
-    for i in string:
-        if i in alpha_dict:
-            alpha_dict[i] += 1
-    return alpha_dict
 
 main()
