42508b21671ec1e6c7a65e720df6b354599d2226
sjehrick <samuel.ehrick@gmail.com>
2025-04-05 19:36:14 -0400

    updated main to use argv for book path

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-from stats import get_num_words
+from stats import *
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
-    print()
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
 
     for item in chars_sorted_list:
         if not item["char"].isalpha():
             continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
+        print(f"{item["char"]}: {item['num']}")
 
-    print("--- End report ---")
+    print("============= END ===============")
 
 def sort_on(d):
     return d["num"]
@@ -30,15 +37,7 @@ def chars_dict_to_sorted_list(num_chars_dict):
     return sorted_list
 
 
-def get_chars_dict(text):
-    chars = {}
-    for c in text:
-        lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
-        else:
-            chars[lowered] = 1
-    return chars
+
 
 
 def get_book_text(path):
